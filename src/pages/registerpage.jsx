41f34cf8f3081c2dc4eb72cf87413c2e5795a098
Santhosh0809 <112320593+Santhosh0809@users.noreply.github.com>
2022-08-30 00:21:01 +0530

    Update registerpage.jsx

--- src/pages/registerpage.jsx
+++ src/pages/registerpage.jsx
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 class Registerpage extends Component {
     state = {  } 
     render() { 
         return (
             <div>
-                <nav className="navbar"></nav>
-    <div className="nav">
-        <img src="./assets/img/Logo.svg" className="brand logo" alt=""/>
-        <div class="reg/login">
-        <h1 className='provide-details'>Register to Start Shopping</h1>
+                <nav className="regnavbar"></nav>
+    <div className="regnav">
+    <Link to="/"><img src="./assets/img/Logo.svg" className="hombrand logo" alt=""/></Link>
+        <div className="regreg/login">
+        <h1 className='regprovide-details'>Register to Start Shopping</h1>
 
         </div>
     </div>
-    <div className='box-container'>
-    <div class="login-details">
-        <label for="Email" className="emaillabel">Email</label><br></br>
-        <input type="text" className="email" placeholder="Email"/><br></br>
-        <label for="password" className="passwordlabel">Password</label><br></br>
-        <input type="password" className="passwd" placeholder="Enter Password"/><br></br>
-        <label for="password" className="Confirmpasswordlabel">Confirm Password</label><br></br>
-        <input type="password" className="repasswd" placeholder="Re-Enter Password"/><br></br>
-        <button className="bigregbtn">REGISTER</button>
+    <div className='regbox-container'>
+    <div class="reglogin-details">
+        <label for="Email" className="regemaillabel">Email</label><br></br>
+        <input type="text" className="regemail" placeholder="Email"/><br></br>
+        <label for="password" className="regpasswordlabel">Password</label><br></br>
+        <input type="password" className="regpasswd" placeholder="Enter Password"/><br></br>
+        <label for="password" className="regConfirmpasswordlabel">Confirm Password</label><br></br>
+        <input type="password" className="regrepasswd" placeholder="Re-Enter Password"/><br></br>
+        <button className="regbigregbtn">REGISTER</button>
     </div>
-      <div className="bgimg">
-         <img src="./assets/img/banner - bg.svg" alt="" className="orangebg"/>
-          <img src="./assets/img/banner - product.svg" alt="" className="probanner"/>
+      <div className="regbgimg">
+         <img src="./assets/img/banner - bg.svg" alt="" className="regorangebg"/>
+          <img src="./assets/img/banner - product.svg" alt="" className="regprobanner"/>
        </div>  
     </div>
     </div>
