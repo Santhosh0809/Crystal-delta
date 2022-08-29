cf1bba5c45cc9341cee4959af4f234ffee9adb8a
Santhosh0809 <112320593+Santhosh0809@users.noreply.github.com>
2022-08-30 00:19:51 +0530

    Update products.jsx

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 
@@ -9,233 +7,233 @@ class Productpage extends Component{
     render() { 
         return (
             <div>
-                <div className="navbar">
-        <img src="./img/Logo.svg" alt=""/>
-        <div className="sellprobtn">
-        <button className="sellbtn">+ SELL</button>
-        <button className="profile"><img src="./assets/img/circle-user.svg" alt="" width="40px" height="40px"/></button>
+                <div className="pronavbar">
+                <Link to="/"><img src="./assets/img/Logo.svg" className="hombrand logo" alt=""/></Link>
+        <div className="prosellprobtn">
+        <button className="prosellbtn">+ SELL</button>
+        <button className="proprofile"><img src="./assets/img/circle-user.svg" alt="" width="40px" height="40px"/></button>
     </div>
     </div>
-    <div className="nav">
-        <ul className="links-container">
-            <li className="links"><Link to="" className="items">All</Link ></li>
-            <li className="links"><Link to="" className="items">Car</Link ></li>
-            <li className="links"><Link to="" className="items">Furniture</Link ></li>
-            <li className="links"><Link to="" className="items">Mobile</Link ></li>
-            <li className="links"><Link to="" className="items">TV's</Link ></li>
-            <li className="links"><Link to="" className="items">Kitchen appliances</Link ></li>
-            <li className="links"><Link to="" className="items">Laptops</Link ></li>
-            <li className="links"><Link to="" className="items">Home appliances</Link ></li>
-            <li className="links"><Link to="" className="items">Entertainment</Link ></li>
+    <div className="pronav">
+        <ul className="prolinks-container">
+            <li className="prolinks"><Link to="" className="proitems">All</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Car</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Furniture</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Mobile</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">TV's</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Kitchen appliances</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Laptops</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Home appliances</Link ></li>
+            <li className="prolinks"><Link to="" className="proitems">Entertainment</Link ></li>
 
         </ul>
     </div>
-    <div className="box-container" >
-        <Link to=""><div className ="box">
-            <div className="image">
+    <div className="probox-container" >
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/watch.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Fastrack Watch</h3>
-                <p className="prddetail">Stylish Men's Watch</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+            <div className="proinfo">
+                <h3 className="protitle">Fastrack Watch</h3>
+                <p className="proprddetail">Stylish Men's Watch</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link >
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/rayban.jpg" alt="" width="350px" height="350px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Rayban Glasses</h3>
-                <p className="prddetail">Stylish Unisex Glasses from rayban</p>
+            <div className="proinfo">
+                <h3 className="protitle">Rayban Glasses</h3>
+                <p className="proprddetail">Stylish Unisex Glasses from rayban</p>
 
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/nike.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Nike Shoes</h3>
-                <p className="prddetail">Stylish Unisex Shoes from Nike</p>
+            <div className="proinfo">
+                <h3 className="protitle">Nike Shoes</h3>
+                <p className="proprddetail">Stylish Unisex Shoes from Nike</p>
 
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/bag combo.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Travel bags</h3>
-                <p className="prddetail">(Combo) Pack of 2 bags</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Travel bags</h3>
+                <p className="proprddetail">(Combo) Pack of 2 bags</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/gucci bag.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Gucc1 Hand bag</h3>
-                <p className="prddetail">A premium handbag from gucci</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Gucc1 Hand bag</h3>
+                <p className="proprddetail">A premium handbag from gucci</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/headphones.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">JBL Headphones</h3>
-                <p className="prddetail">The featurestic headphones from JBL</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">JBL Headphones</h3>
+                <p className="proprddetail">The featurestic headphones from JBL</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/landingAccessories.png" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Landing Accessories</h3>
-                <p className="prddetail">A Pack of all Landing Accessories</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Landing Accessories</h3>
+                <p className="proprddetail">A Pack of all Landing Accessories</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/mac makeup.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Mac Makeup</h3>
-                <p className="prddetail">A Pack of Makeupkit</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Mac Makeup</h3>
+                <p className="proprddetail">A Pack of Makeupkit</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/pendant.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Pendant</h3>
-                <p className="prddetail">Premium rustfree pendant</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Pendant</h3>
+                <p className="proprddetail">Premium rustfree pendant</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/sunglass.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Combo Sunglasses</h3>
-                <p className="prddetail">Pack of 2 Sunglasses</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Combo Sunglasses</h3>
+                <p className="proprddetail">Pack of 2 Sunglasses</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+       <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/Smart Watch.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">SMART Watch</h3>
-                <p className="prddetail">Full featured Smartwatch</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">SMART Watch</h3>
+                <p className="proprddetail">Full featured Smartwatch</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
-        <Link to=""><div className ="box">
-            <div className="image">
+        </div>
+        <div className ="probox">
+            <div className="proimage">
               <img src="./assets/img/Alexa.jpg" alt="" width="350px" height="350 px"/>
             </div>
-            <div className="info">
-                <h3 className="title">Bluetooth Speaker</h3>
-                <p className="prddetail">Bluetooth Speaker with best sound quality</p>
-                <div className="subinfo">
-                    <div className="price">₹1000/-</div>
-                    <div><button className="buybtn">BUY PRODUCT</button></div>
+            <div className="proinfo">
+                <h3 className="protitle">Bluetooth Speaker</h3>
+                <p className="proprddetail">Bluetooth Speaker with best sound quality</p>
+                <div className="prosubinfo">
+                    <div className="proprice">₹1000/-</div>
+                    <div><button className="probuybtn">BUY PRODUCT</button></div>
 
-                    <div className="stars">
-                        <i className="fas fa-star  "></i>
+                    <div className="prostars">
+                        <i className="profas fa-star  "></i>
                     </div>
                 </div>
             </div>
-        </div></Link>
+        </div>
     </div>
             </div>
         );
@@ -243,3 +241,4 @@ class Productpage extends Component{
 }
  
 export default Productpage
+
