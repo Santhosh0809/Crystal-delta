8bb9a78dc8c543074199e2516258f9b27813801f
Santhosh0809 <112320593+Santhosh0809@users.noreply.github.com>
2022-08-30 00:17:46 +0530

    Update loginpage.jsx

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 class Loginpag extends Component {
     state = {  } 
     render() { 
         return (
             <div>
-                <nav className="navbar"></nav>
-    <div className="nav">
-        <img src="./assets/img/Logo.svg" className="brand logo" alt=""/>
-        <div class="reg/login">
-        <h1 className='provide-details'>Login to begin your Shopping</h1>
-
-        </div>
+                <nav className="lognavbar"></nav>
+     <div className="lognav">
+    <Link to="/"><img src="./assets/img/Logo.svg" className="hombrand logo" alt=""/></Link>
+        <div className="logreg/login">
+        <h1 className='logprovide-details'>Login to begin your Shopping</h1>
+     </div>
     </div>
-    <div className='box-container'>
-    <div class="login-details">
-        <label for="Email" className="emaillabel">Email</label><br></br>
-        <input type="text" className="email" placeholder="Email"/><br></br>
-        <label for="password" className="passwordlabel">Password</label><br></br>
-        <input type="password" className="passwd" placeholder="Enter Password"/><br></br>
-        <button className="bigloginbtn">LOGIN</button>
+    <div className='logbox-container'>
+    <div class="loglogin-details">
+        <label for="Email" className="logemaillabel">Email</label><br></br>
+        <input type="text" className="logemail" placeholder="Email"/><br></br>
+        <label for="password" className="logpasswordlabel">Password</label><br></br>
+        <input type="password" className="logpasswd" placeholder="Enter Password"/><br></br>
+        <button className="logbigloginbtn">LOGIN</button>
     </div>
-      <div className="bgimg">
-         <img src="./assets/img/banner - bg.svg" alt="" className="orangebg"/>
-          <img src="./assets/img/banner - product.svg" alt="" className="probanner"/>
+      <div className="logbgimg">
+         <img src="./assets/img/banner - bg.svg" alt="" className="logorangebg"/>
+          <img src="./assets/img/banner - product.svg" alt="" className="logprobanner"/>
        </div>  
     </div>
     </div>
