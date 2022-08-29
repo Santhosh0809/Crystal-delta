ab63afb02acc4a3da5c7d6d157b82b0c1f4c524f
Santhosh0809 <112320593+Santhosh0809@users.noreply.github.com>
2022-08-30 00:15:23 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom"
 class Home extends Component {
     state = {  } 
     render() { 
         return (
             <div >
-            <nav className="navbar"></nav>
-    <div class="nav">
-        <img src="./assets/img/Logo.svg" className="brand logo" alt=""/>
-        <div className="reg/login">
-            <button className="loginbtn">Login</button>
-            <button className="regbtn">Register</button>
+            <nav className="homnavbar"></nav>
+    <div class="homnav">
+        <Link to="/"><img src="./assets/img/Logo.svg" className="hombrand logo" alt=""/></Link>
+        <div className="homreg/login">
+            <Link to="/Loginpag"><button className="homloginbtn">Login</button></Link>
+            <Link to="/Registerpage"><button className="homregbtn">Register</button></Link>
         </div>
     </div>
-    <div className='box-container'>
-    <div className="bgimg">
-     <img src="./assets/img/banner - bg.svg" alt="" className="orangebg"/>
-     <img src="./assets/img/banner - bg -blue.svg" alt="" className="bluebg"/> 
+    <div className='hombox-container'>
+    <div className="hombgimg">
+     <img src="./assets/img/banner - bg.svg" alt="" className="homorangebg"/>
+     <img src="./assets/img/banner - bg -blue.svg" alt="" className="hombluebg"/> 
      </div>
-     <img src="./assets/img/banner - product.svg" alt="" className="probanner"/>  
-     <div className='tex'>
-     <div class="texts">
-        <h1 className="firsttxt">BUY&nbsp;</h1>
-        <h1 className="secondtxt">THE&nbsp; </h1>
-        <h1 className="thirdtxt">PRODUCT&nbsp; </h1>
-        <h1 className="forthtxt">FOR&nbsp;</h1>
-        <h1 className="fifthtxt">YOU!&nbsp;</h1>
+     <img src="./assets/img/banner - product.svg" alt="" className="homprobanner"/>  
+     <div className='homtex'>
+     <div class="homtexts">
+        <h1 className="homfirsttxt">BUY&nbsp;</h1>
+        <h1 className="homsecondtxt">THE&nbsp; </h1>
+        <h1 className="homthirdtxt">PRODUCT&nbsp; </h1>
+        <h1 className="homforthtxt">FOR&nbsp;</h1>
+        <h1 className="homfifthtxt">YOU!&nbsp;</h1>
      </div>  
-     <div className="ndtexts">
-        <h1 className="sixthtxt">SELL&nbsp;</h1>
-        <h1 className="seventhxt">YOUR&nbsp;</h1>
-        <h1 className="eighthtxt">PRODUCTS!&nbsp;</h1>
+     <div className="homndtexts">
+        <h1 className="homsixthtxt">SELL&nbsp;</h1>
+        <h1 className="homseventhxt">YOUR&nbsp;</h1>
+        <h1 className="homeighthtxt">PRODUCTS!&nbsp;</h1>
     </div>
-    <div className="smltxt">
-        <h3 className="st">Lorem Ipsum is simply dummy text of the</h3>
+    <div className="homsmltxt">
+        <h3 className="homst">Lorem Ipsum is simply dummy text of the</h3>
     </div>
-     <div className="smltxt2"><h3>printing and typesetting industry.</h3></div>
-<button className="expbtn">Explore products</button>
+     <div className="homsmltxt2"><h3>printing and typesetting industry.</h3></div>
+<Link to="/Productpage"><button className="homexpbtn">Explore products</button></Link>
      </div>
      </div>
      </div>
